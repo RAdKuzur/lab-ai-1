70a5d7d7504890f6818ed350cda2fac9ada431a3
kuzurgalievra <kuzurgalievra@teletribe.ru>
2026-02-20 13:20:28 +0400

    version without nginx & fix redirect system

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\AIController;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+    return redirect()->route('ai.index');
+});
+
 
 Route::get('/ai', [AiController::class, 'index'])->name('ai.index');
 Route::post('/chat', [AiController::class, 'chat'])->name('ai.chat');
